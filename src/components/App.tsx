8e4a7202a5364ec567f21fd98a19bd31b3d67c56
Jonathan Sabbe <jhon287@users.noreply.github.com>
2023-09-01 23:26:08 +0200

    feat: add code base (#1)

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import Fact from './Fact';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='title'>Meow Facts!</h1>
+      <Fact />
+    </div>
+  );
+}
+
+export default App;
